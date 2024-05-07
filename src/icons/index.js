aa1c5ec8835b6aceadc2db5112ce8a22a0748200
toancodedao <105698648+toancodedao@users.noreply.github.com>
2024-05-07 22:17:00 +0700

    [0.0.1][feature/Login] - Add fontsize , colors , icon in theme

---
+++ src/icons/index.js
@@ -0,0 +1,4 @@
+import Logo from './Logo.svg';
+import EyeHide from './EyeHide.svg';
+
+export {Logo, EyeHide};
---
+++ src/theme/colors.js
@@ -0,0 +1,10 @@
+export default {
+  white: '#FFFFFF',
+
+  black: '#000000',
+
+  green_004643: '#004643',
+
+  gray_757575: '#757575',
+  gray_C4C4C4: '#C4C4C4',
+};
